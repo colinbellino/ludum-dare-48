6768abd6f8427871735108df927114408bfbbf85
Colin Bellino <bellino.colin@gmail.com>
2021-04-24 06:06:50 +0200

    Add platformer movement and basic tileset and collisions

--- Assets/_Code/Game.Core/Components/EntityComponent.cs
+++ Assets/_Code/Game.Core/Components/EntityComponent.cs
@@ -1,22 +1,23 @@
 ﻿using UnityEngine;
+using Prime31;
 
 namespace Game.Core
 {
-	public enum Alliances { Foe, Ally }
-	public enum Brain { Player, Helper, Shooter, Roamer }
-
 	[SelectionBase]
 	public class EntityComponent : MonoBehaviour
 	{
-		[SerializeField] public int BaseHealth = 3;
-		[SerializeField] public float MoveSpeed = 10f;
-		[SerializeField] public Alliances Alliance;
-		[SerializeField] public Brain Brain;
-		[SerializeField] public AudioClip FireClip;
-		[SerializeField] public AudioClip DamagedClip;
-		[SerializeField] public AudioClip DestroyedClip;
 		[SerializeField] public Rigidbody2D Rigidbody;
+		[SerializeField] public CharacterController2D Controller;
+		[SerializeField] public Animator Animator;
 
-		[HideInInspector] public int CurrentHealth;
+		[SerializeField] public float Gravity = -25f;
+		[SerializeField] public float RunSpeed = 8f;
+		[SerializeField] public float GroundDamping = 20f;
+		[SerializeField] public float InAirDamping = 5f;
+		[SerializeField] public float JumpHeight = 3f;
+
+		[HideInInspector] public RaycastHit2D LastControllerColliderHit;
+		[HideInInspector] public Vector3 Velocity;
+		[HideInInspector] public float NormalizedHorizontalSpeed = 0;
 	}
 }
--- Assets/_Code/Game.Core/StateMachine/GameplayState.cs
+++ Assets/_Code/Game.Core/StateMachine/GameplayState.cs
@@ -7,6 +7,8 @@ namespace Game.Core
 {
 	public class GameplayState : BaseGameState
 	{
+		private bool _confirmWasPressedThisFrame;
+
 		public GameplayState(GameFSM machine, Game game) : base(machine, game) { }
 
 		public override async UniTask Enter()
@@ -18,22 +20,23 @@ namespace Game.Core
 			_ui.SetDebugText(@"State: Gameplay
 - F1: Trigger win condition
 - F2: Trigger defeat condition");
-			_state.Player = SpawnPlayer(_config.PlayerPrefab, _game, Vector3.zero);
-			if (_state.Background == null)
-			{
-				_state.Background = GameObject.Find("parallax_mockup");
-			}
-			_state.Background.SetActive(true);
+			_state.Player = SpawnPlayer(_config.PlayerPrefab, _game, new Vector3(0f, 12f, 0));
 			_controls.Gameplay.Enable();
+			_controls.Gameplay.Confirm.started += ConfirmStarted;
 
 			_ = _audioPlayer.PlayMusic(_config.MainMusic);
 
 			await _ui.FadeOut();
 		}
 
+		private void ConfirmStarted(InputAction.CallbackContext context) => _confirmWasPressedThisFrame = true;
+
 		public override void Tick()
 		{
 			base.Tick();
+
+			var moveInput = _controls.Gameplay.Move.ReadValue<Vector2>();
+			var confirmInput = _controls.Gameplay.Confirm.ReadValue<float>();
 
 			if (IsDevBuild())
 			{
@@ -49,13 +52,78 @@ namespace Game.Core
 
 			if (_state.Player != null)
 			{
-				var speed = 5f;
-				var moveInput = _controls.Gameplay.Move.ReadValue<Vector2>();
-				var position = _state.Player.transform.position;
-				position.x += moveInput.x * Time.deltaTime * speed;
-				position.y += moveInput.y * Time.deltaTime * speed;
-				_state.Player.transform.position = position;
+				var player = _state.Player;
+				if (player.Controller.isGrounded)
+				{
+					player.Velocity.y = 0;
+				}
+
+				if (moveInput.x > 0f)
+				{
+					player.NormalizedHorizontalSpeed = 1;
+					if (player.transform.localScale.x < 0f)
+					{
+						player.transform.localScale = new Vector3(-player.transform.localScale.x, player.transform.localScale.y, player.transform.localScale.z);
+					}
+
+					if (player.Controller.isGrounded)
+					{
+						player.Animator.Play(Animator.StringToHash("Run"));
+					}
+				}
+				else if (moveInput.x < 0f)
+				{
+					player.NormalizedHorizontalSpeed = -1;
+					if (player.transform.localScale.x > 0f)
+					{
+						player.transform.localScale = new Vector3(-player.transform.localScale.x, player.transform.localScale.y, player.transform.localScale.z);
+					}
+
+					if (player.Controller.isGrounded)
+					{
+						player.Animator.Play(Animator.StringToHash("Run"));
+					}
+				}
+				else
+				{
+					player.NormalizedHorizontalSpeed = 0;
+
+					if (player.Controller.isGrounded)
+					{
+						player.Animator.Play(Animator.StringToHash("Idle"));
+					}
+				}
+
+
+				// we can only jump whilst grounded
+				if (player.Controller.isGrounded && _confirmWasPressedThisFrame)
+				{
+					player.Velocity.y = Mathf.Sqrt(2f * player.JumpHeight * -player.Gravity);
+					player.Animator.Play(Animator.StringToHash("Jump"));
+				}
+
+				// apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
+				var smoothedMovementFactor = player.Controller.isGrounded ? player.GroundDamping : player.InAirDamping; // how fast do we change direction?
+				player.Velocity.x = Mathf.Lerp(player.Velocity.x, player.NormalizedHorizontalSpeed * player.RunSpeed, Time.deltaTime * smoothedMovementFactor);
+
+				// apply gravity before moving
+				player.Velocity.y += player.Gravity * Time.deltaTime;
+
+				// // if holding down bump up our movement amount and turn off one way platform detection for a frame.
+				// // this lets us jump down through one way platforms
+				// if (player.Controller.isGrounded && moveInput.y < 0f)
+				// {
+				// 	player.Velocity.y *= 3f;
+				// 	player.Controller.ignoreOneWayPlatformsThisFrame = true;
+				// }
+
+				player.Controller.move(player.Velocity * Time.deltaTime);
+
+				// grab our current player.Velocity to use as a base for all calculations
+				player.Velocity = player.Controller.velocity;
 			}
+
+			_confirmWasPressedThisFrame = false;
 		}
 
 		public override async UniTask Exit()
@@ -64,9 +132,9 @@ namespace Game.Core
 
 			_ui.HideGameplay();
 			GameObject.Destroy(_state.Player.gameObject);
-			_state.Background.SetActive(false);
 
 			_controls.Gameplay.Disable();
+			_controls.Gameplay.Confirm.started -= ConfirmStarted;
 		}
 
 		private async void Victory()
